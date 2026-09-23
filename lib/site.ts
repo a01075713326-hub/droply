6a248235d85e566d59860d0ba13c5dee86e21e66
root <root@droply.digital>
2026-09-23 20:56:06 +0000

    chore: update contact email

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   name: "Droply",
   url: "https://droply.digital",
-  email: "officialcarl32@gmail.com",
+  email: "droply.digital@gmail.com",
   telegram: "droplysup",
   updated: "September 20, 2026",
 };
