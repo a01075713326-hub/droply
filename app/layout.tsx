611148ec50c01878ac5709a1d2fb81a926ecabc0
root <root@droply.digital>
2026-09-26 21:34:41 +0000

    feat: add Microsoft Clarity and self-hosted analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -30,6 +31,19 @@ export default function RootLayout({ children }: Readonly<{children: React.React
     <html lang="en" className={inter.variable}>
       <head>
         <script type="module" src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "4154b45fac604535bd8729e76bf00677"}'></script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "yo3syd0u0p");`}
+        </Script>
+        <Script
+          async
+          src="https://analytics.droply.digital/script.js"
+          data-website-id="658809ad-5abd-4357-9858-bb12ec374311"
+          strategy="afterInteractive"
+        />
       </head>
       <body><AuroraBackground/><Header/>{children}<Footer/></body>
     </html>
